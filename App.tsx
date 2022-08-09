35a8b4ca9cb986a8572fbbfc5513710b615cf0da
mac-h95 <macasmac@protonmail.com>
2022-08-09 09:20:16 +0100

    added loader component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,28 @@
 import * as React from 'react';
 import './style.css';
 import { useState } from 'react';
+import useSWR from 'swr';
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+function fetchPokemon() {
+  const { data, error } = useSWR('https://pokeapi.co/api/v2/pokemon', fetcher);
+
+  return {
+    data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+}
 
 export default function App() {
   return (
-    <main>
+    <div id="app-container">
       <Header />
       <Container />
+      <Spacer />
       <Footer />
-    </main>
+    </div>
   );
 }
 
@@ -16,18 +30,31 @@ function Header() {
   return (
     <header>
       <a href="/">
-        <h1>Pokemon</h1>
+        <h1>Pokémon</h1>
       </a>
     </header>
   );
 }
 
 function Container() {
+  const { data, isLoading, isError } = fetchPokemon();
+  console.log(data);
   return (
-    <div>
-      <Menu />
-      <List />
-    </div>
+    <main>
+      <Loader />
+      {/* {isError && (
+        <div id="error">
+          <h1>Oops...</h1>
+          <p>Contact support if this error persists.</p>
+        </div>
+      )}
+      {data && (
+        <div className="container">
+          <Menu />
+          <List />
+        </div>
+      )} */}
+    </main>
   );
 }
 
@@ -40,11 +67,17 @@ function Menu() {
       <button>{currentGen}</button>
       <ul id="generation-menu">
         {generations.map((generation) => (
-          <li onClick={() => setCurrentGen(generation)}>{generation}</li>
+          <li key={generation} onClick={() => setCurrentGen(generation)}>
+            {generation}
+          </li>
         ))}
       </ul>
     </div>
   );
+}
+
+function Loader() {
+  return <div className="loader" />;
 }
 
 function List() {
@@ -62,6 +95,10 @@ function Item({ name }) {
       <h2>{name}</h2>
     </div>
   );
+}
+
+function Spacer() {
+  return <div className="spacer" />;
 }
 
 function Footer() {
