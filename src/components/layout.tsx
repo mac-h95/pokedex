34d2cf7483defab55f3c37075c40b58d05da0c0b
mac-h95 <macasmac@protonmail.com>
2022-08-12 09:03:38 +0100

    added favicon and converted logo component into img tag

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -21,7 +21,7 @@ function Header({ view, setView }) {
   return (
     <header>
       <a href="/">
-        <Pokeball />
+        <img src="/logo.svg" alt="Pokéball logo" />
         <h1>Pokédex</h1>
       </a>
       <nav>
@@ -45,32 +45,6 @@ function Header({ view, setView }) {
         </a>
       </nav>
     </header>
-  );
-}
-
-function Pokeball() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="100px" height="100px">
-      <path
-        d="M 30 50
-		a 1 1 1 0 1 40 0
-		h-12.5
-		a 1 1 1 0 0 -15 0
-		z"
-        fill="#f00"
-        stroke="#222"
-      ></path>
-      <circle cx="50" cy="50" r="5" fill="#222" stroke="#222"></circle>
-      <path
-        d="M 30 50
-		a 1 1 1 0 0 40 0
-		h-12.5
-		a 1 1 1 0 1 -15 0
-		z"
-        fill="#fff"
-        stroke="#222"
-      ></path>
-    </svg>
   );
 }
 
